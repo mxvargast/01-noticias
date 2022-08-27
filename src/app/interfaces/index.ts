98bb0b5f7c7c5f44731470b06a25b5ef86986fea
mxvargast <maximiliano.varta@gmail.com>
2022-08-27 00:03:11 -0400

    segundo commit

---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,29 @@
+export interface NewsResponse {
+    status:       string;
+    totalResults: number;
+    articles:     Article[];
+}
+
+export interface Article {
+    source:      Source;
+    author:      string;
+    title:       string;
+    description: string;
+    url:         string;
+    urlToImage:  string;
+    publishedAt: Date;
+    content:     string;
+}
+
+export interface Source {
+    id:   ID;
+    name: Name;
+}
+
+export enum ID {
+    Techcrunch = "techcrunch",
+}
+
+export enum Name {
+    TechCrunch = "TechCrunch",
+}
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { NewsResponse } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getTopHeadLines(){
+    return this.http.get<NewsResponse>('https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=6ff46ef6db2e46068fe1fb8b57df2ebe')
+  }
+
 }
